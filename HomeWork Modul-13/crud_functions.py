daa7b3fe69d96ed236de6e7a94658d932482f3ca
Rashid20071601 <153351715+Rashid20071601@users.noreply.github.com>
2024-12-15 08:19:19 +0300

    Add files via upload

---
+++ HomeWork Modul-13/crud_functions.py
@@ -0,0 +1,58 @@
+# Import
+import sqlite3
+
+
+
+def initiate_db():
+  connection = sqlite3.connect('database.db')
+  cursor = connection.cursor()
+
+  cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INT NOT NULL
+    );
+  ''')
+
+  connection.commit()
+  connection.close()
+
+
+initiate_db()
+
+
+
+def get_all_products():
+  connection = sqlite3.connect('database.db')
+  cursor = connection.cursor()
+
+  cursor.execute('SELECT * FROM Products')
+  products = cursor.fetchall()
+
+  cursor.close()
+  connection.close()
+
+  return products
+
+
+
+def insert_products():
+  connection = sqlite3.connect('database.db')
+  cursor = connection.cursor()
+
+  products = [
+    ('Продукт 1', 'Описание 1', 100),
+    ('Продукт 2', 'Описание 2', 200),
+    ('Продукт 3', 'Описание 3', 300),
+    ('Продукт 4', 'Описание 4', 400)
+  ]
+
+  cursor.executemany('INSERT OR IGNORE INTO Products(title, description, price) VALUES(?, ?, ?)', products)
+
+  connection.commit()
+  connection.close()
+
+
+# insert_products()
